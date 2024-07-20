922101f1ef52880da68a3b608403d41219d363ba
Pratyush Pavan <pratyushpavanchoudhary@gmail.com>
2024-07-20 23:42:12 +0530

    search feature added to query dashboard

--- src/Component/QueryDashboard.js
+++ src/Component/QueryDashboard.js
@@ -1,23 +1,44 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import "./Dashboard2.css";
 import { useNavigate } from "react-router-dom";
 
 const QeryDashboard = ({ forms,setEditQeryFormData }) => {
+  const [searchUID, setSearchUID] = useState("");
+  const [QuertFormtList, setQueryFormList] = useState([]);
   const navigate = useNavigate();
+
   const handleEdit = (form) => {
     setEditQeryFormData(form);
     navigate("/queryeditForm");
   };
+
+  useEffect(() => {
+    if(searchUID.length){
+       const filteredForms=forms.filter((form)=>form.uid.toLowerCase().includes(searchUID.toLocaleLowerCase()))
+       setQueryFormList(filteredForms)
+    }
+    else{
+      setQueryFormList(forms)
+    }
+  }, [forms,searchUID]);
 
   return (
     <div className="dashboard">
       <h1 style={{ color: "black", backgroundColor: "AppWorkspace" }}>
         Query Dashboard
       </h1>
+      <div className="search-bar">
+        <label style={{textAlign:'center'}}>Search by UID</label>
+        <input
+          type="text"
+          value={searchUID}
+          onChange={(e) => setSearchUID(e.target.value)}
+        />
+      </div>
       <div className="total-itineraries">
         <p>Total Query: {forms.length}</p>
       </div>
-      {forms?.length === 0 ? (
+      {QuertFormtList?.length === 0 ? (
         <p>No forms submitted yet.</p>
       ) : (
         <table>
@@ -34,7 +55,7 @@ const QeryDashboard = ({ forms,setEditQeryFormData }) => {
             </tr>
           </thead>
           <tbody>
-            {forms?.map((form, index) => (
+            {QuertFormtList?.map((form, index) => (
               <tr key={index}>
                 <td>{form.uid}</td>
                 <td>{form.name}</td>
