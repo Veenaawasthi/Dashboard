0e7188f51a9976c15fc7b51f667d9ef072598a29
Pratyush Pavan <pratyushpavanchoudhary@gmail.com>
2024-07-20 21:42:12 +0530

    search feature enable

--- src/Component/Dashboard2.js
+++ src/Component/Dashboard2.js
@@ -1,51 +1,61 @@
-import React from 'react';
-import './Dashboard2.css';
+import React, { useEffect, useState } from "react";
+import "./Dashboard2.css";
 import { useNavigate } from "react-router-dom";
 
-const Dashboard = ({ forms,setItineraryData,setEditFormData}) => {
-  // const [searchName, setSearchName] = useState('');
-  // const [searchFileCode, setSearchFileCode] = useState('');
+const Dashboard = ({ forms, setItineraryData, setEditFormData }) => {
+  const [searchName, setSearchName] = useState("");
+  const [searchFileCode, setSearchFileCode] = useState("");
+  const [formList, setFormList] = useState([]);
   const navigate = useNavigate();
 
   // Function to handle editing a form
   const handleEdit = (form) => {
-    setEditFormData(form)
-    navigate('/editForm')
+    setEditFormData(form);
+    navigate("/editForm");
   };
 
-
   // Filter forms based on search inputs
-  // const filteredForms = forms.filter(form => {
-  //   const nameMatch = form.name ? form.name.toLowerCase().includes(searchName.toLowerCase()) : false;
-  //   const fileCodeMatch = form.filecode ? form.filecode.toLowerCase().includes(searchFileCode.toLowerCase()) : false;
-  //   return nameMatch && fileCodeMatch;
-  // });
+  useEffect(() => {
+    if(searchName.length){
+       const filteredForms=forms.filter((form)=>form.clientName.toLowerCase().includes(searchName.toLocaleLowerCase()))
+       setFormList(filteredForms)
+    }else if(searchFileCode.length){
+      const filteredForms=forms.filter((form)=>form.fileCode.toLowerCase().includes(searchFileCode.toLocaleLowerCase()))
+      setFormList(filteredForms)
+    }
+    else{
+      setFormList(forms)
+    }
+  }, [forms,searchFileCode,searchName]);
+
   const handleView = (form) => {
-    setItineraryData(form)
-    navigate('/view')
+    setItineraryData(form);
+    navigate("/view");
   };
 
   return (
     <div className="dashboard">
-      <h1 style={{ color: 'black', backgroundColor: "AppWorkspace" }}>Itinerary Dashboard</h1>
-      {/* <div className="search-bar">
-        <label>Search by Name</label>
+      <h1 style={{ color: "black", backgroundColor: "AppWorkspace" }}>
+        Itinerary Dashboard
+      </h1>
+      <div className="search-bar">
+        <label style={{textAlign:'center'}}>Search by Name</label>
         <input
           type="text"
           value={searchName}
           onChange={(e) => setSearchName(e.target.value)}
         />
-        <label>Search by File Code</label>
+        <label style={{textAlign:'center'}}>Search by File Code</label>
         <input
           type="text"
           value={searchFileCode}
           onChange={(e) => setSearchFileCode(e.target.value)}
         />
-      </div> */}
+      </div>
       <div className="total-itineraries">
         <p>Total Itineraries: {forms.length}</p>
       </div>
-      {forms?.length === 0 ? (
+      {formList?.length === 0 ? (
         <p>No forms submitted yet.</p>
       ) : (
         <table>
@@ -61,7 +71,7 @@ const Dashboard = ({ forms,setItineraryData,setEditFormData}) => {
             </tr>
           </thead>
           <tbody>
-            {forms?.map((form, index) => (
+            {formList?.map((form, index) => (
               <tr key={index}>
                 <td>{form.fileCode}</td>
                 <td>{form.clientName}</td>
@@ -70,7 +80,9 @@ const Dashboard = ({ forms,setItineraryData,setEditFormData}) => {
                 <td>{form.tourDate}</td>
                 <td>{form.flight}</td>
                 <td>
-                  <button onClick={() => handleView(form)}>View Form Details</button>
+                  <button onClick={() => handleView(form)}>
+                    View Form Details
+                  </button>
                   <button onClick={() => handleEdit(form)}>Edit</button>
                 </td>
               </tr>
@@ -78,22 +90,8 @@ const Dashboard = ({ forms,setItineraryData,setEditFormData}) => {
           </tbody>
         </table>
       )}
-
-      {/* {editForm && (
-        <div className="edit-form">
-          <h2>Edit Form</h2>
-          <Form
-            formData={editForm}
-            onSubmit={handleUpdateForm}
-            onCancel={handleCancelEdit}
-          />
-        </div>
-      )} */}
     </div>
   );
 };
 
 export default Dashboard;
-
-
-
